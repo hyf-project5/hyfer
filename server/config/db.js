a367c78192e09fd2799ea8903f0a1b075adc98c3
hasanshahoud <hasansh343@gmail.com>
2017-02-21 21:25:27 +0100

    configure the to config.sample file

--- server/config/db.js
+++ server/config/db.js
@@ -2,13 +2,13 @@
 
 const mysql = require('mysql');
 let connection = require('express-myconnection');
-const config = require('./config');
-module.exports = function (app) {
-  app.use(connection(mysql, {
-      host: config.host,
-      user: config.user,
-      password: config.password,
-      database: config.database
-    }, 'pool') //or single
-  );
+const config = require('./config.sample');
+module.exports = function(app) {
+    app.use(connection(mysql, {
+            host: config.host,
+            user: config.user,
+            password: config.password,
+            database: config.database
+        }, 'pool') //or single
+    );
 };
