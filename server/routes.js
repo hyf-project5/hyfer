8606d945cec2c504ac5e6e0d9bac6f4551079001
hasanshahoud <hasansh343@gmail.com>
2017-02-23 14:25:28 +0100

    use patch method to update a particular module

--- server/routes.js
+++ server/routes.js
@@ -4,6 +4,7 @@ module.exports = function(app) {
     app.get('/module', module.list);
     app.get('/module/:id', module.module_details);
     app.post('/module', module.add);
+    app.patch('/module/:id', module.update);
     app.delete('/module/:id', module.delete_module);
     // I changed the route to singular, we should definitely agree for which should we use ;)
 };
