4b92b87299c5fa0fd63336f813e5be564a6910c4
Jim Cramer <remarcmij@gmail.com>
2017-03-03 12:08:32 +0100

    added 'api' prefex

--- server/routes.js
+++ server/routes.js
@@ -4,32 +4,25 @@ require('./auth/github-auth');
 const authService = require('./auth/auth-service');
 const github = require('./api/github');
 
-// API requirements
-const modules = require('./api/modules'); // Modules tabel API
-const groups = require('./api/groups'); // Groups 'classes' tabel API
-const config = require('./config/config.js')
+const modules = require('./api/modules');
+const groups = require('./api/groups');
 
 module.exports = function(app) {
 
-    // Routes API
-    // modules HTTP methods
-    app.get('/modules', modules.getCurriculum);
-    app.get('/modules/:id', modules.getModule);
-    app.post('/modules', modules.addModule);
-    app.patch('/modules/:id', modules.updateModule);
-    app.delete('/modules/:id', authService.hasRole('teacher'), modules.deleteModule);
+    app.get('/api/modules', modules.getCurriculum);
+    app.get('/api/modules/:id', modules.getModule);
+    app.post('/api/modules', modules.addModule);
+    app.patch('/api/modules/:id', modules.updateModule);
+    app.delete('/api/modules/:id', authService.hasRole('teacher'), modules.deleteModule);
 
-    // groups HTTP methods
-    app.get('/groups', groups.getTimelineForAllGroups);
-    app.get('/groups/:id', groups.getTimelineForAGroup);
-    app.post('/groups', groups.addGroup);
-    app.patch('/groups/:id', groups.updateGroup);
-    app.delete('/groups/:id', groups.deleteGroup);
+    app.get('/api/groups', groups.getTimelineForAllGroups);
+    app.get('/api/groups/:id', groups.getTimelineForAGroup);
+    app.post('/api/groups', groups.addGroup);
+    app.patch('/api/groups/:id', groups.updateGroup);
+    app.delete('/api/groups/:id', groups.deleteGroup);
 
-    // GitHub module README's
-    app.get('/github/readme/:owner/:repo', github.getReadMeAsHtml);
+    app.get('/api/github/readme/:owner/:repo', github.getReadMeAsHtml);
 
-    // Github authentication
     app.get('/auth/github', passport.authenticate('github'));
     app.get('/auth/github/callback', passport.authenticate('github', { failureRedirect: '/login' }),
         authService.gitHubCallback, authService.setTokenCookie);
