05959ebfc8f01167b850cfd48e6e2f203c7a177d
Jim Cramer <remarcmij@gmail.com>
2017-03-03 12:46:33 +0100

    upgraded express to v4.x rerun `npm install` to update dependencies need to fix an issue with GitHub sign-in

--- server/app.js
+++ server/app.js
@@ -1,43 +1,32 @@
-//Module dependencies.
+'use strict';
 const express = require('express');
-const routes = require('./routes');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+const cookieParser = require('cookie-parser');
+const serveStatic = require('serve-static');
+const errorHandler = require('errorhandler');
 const http = require('http');
 const path = require('path');
 const passport = require('passport');
-const addRequestId = require('express-request-id')();
+const requestId = require('express-request-id')();
 const app = express();
-const cookieParser = require('cookie-parser');
-// const jwt = require('express-jwt');
 
 // all environments
 app.set('port', process.env.PORT || 3002);
 
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride());
 app.use(cookieParser());
 app.use(passport.initialize());
-// app.use(jwt({
-//     secret: 'hyferSecret',
-//     credentialsRequired: false,
-//     getToken: function fromHeaderOrQuerystring(req) {
-//         if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
-//             console.log(req.headers.authorization.split(' ')[1]);
-//             return req.headers.authorization.split(' ')[1];
-//         } else if (req.query && req.query.token) {
-//             return req.query.token;
-//         }
-//         return null;
-//     }
-// }));
+app.use(requestId);
+app.use(serveStatic(path.join(__dirname, '..')));
+app.use(serveStatic(path.join(__dirname, '../client')));
 
-app.use(addRequestId);
-app.use(express.static(path.join(__dirname, '..')));
-app.use(express.static(path.join(__dirname, '../client')));
-// development only
 if ('development' == app.get('env')) {
-    app.use(express.errorHandler());
+    app.use(errorHandler());
 }
 
 require('./config/db')(app);
