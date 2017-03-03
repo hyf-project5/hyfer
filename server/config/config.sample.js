634af457849efd40c455bd98fe08870fd67d2047
Jim Cramer <remarcmij@gmail.com>
2017-03-03 15:42:23 +0100

    fixed oauth issue

--- server/config/config.sample.js
+++ server/config/config.sample.js
@@ -6,5 +6,6 @@ module.exports = {
     clientID: 'github client id',
     clientSecret: 'github client secret',
     callbackURL: 'github callback url',
+    url: 'http://127.0.0.1:3002',
     jwtSecret: 'jwtSecret'
 };
