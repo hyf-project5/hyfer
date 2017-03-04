8e8624160389a8c6ab09ba706c1920c1377dab0c
hasanshahoud <hasansh343@gmail.com>
2017-03-04 21:42:39 +0100

    initialize the readme(html) to be the first module of the first class, and render info of readme to the view

--- client/app/timeline/component.js
+++ client/app/timeline/component.js
@@ -12,11 +12,17 @@
 
     function hyfTimelineController(backendService, $sce) {
         var ctrl = this;
-        ctrl.name = "Hasan sh";
+
         backendService.getTimeline()
             .then(res => {
                 ctrl.timeline = res;
                 ctrl.classes = Object.keys(ctrl.timeline).sort();
+                // for development puposes hard-coded(concatenated) the git_url&git_repo till fixing(until the real data) 
+                let gitUrl = ctrl.timeline[ctrl.classes[0]][0].git_url + ctrl.timeline[ctrl.classes[0]][0].git_repo;
+                ctrl.readme = {
+                    moduleName: ctrl.timeline[ctrl.classes[0]][0].module_name,
+                    gitUrl: gitUrl
+                }
                 ctrl.maxLength = 0;
                 for (let key in ctrl.timeline) {
                     ctrl.maxLength = Math.max(ctrl.maxLength, ctrl.timeline[key].length, 10);
@@ -28,10 +34,14 @@
 
         ctrl.showReadme = showReadme;
 
-        function showReadme(gitRepo) {
-            backendService.getReadme(gitRepo)
+        function showReadme(module) {
+            backendService.getReadme(module.git_repo)
                 .then(res => {
                     ctrl.readmeFile = $sce.trustAsHtml(res);
+                    ctrl.readme = {
+                        moduleName: module.module_name,
+                        gitUrl: module.git_url + module.git_repo
+                    }
                 })
                 .catch(err => console.log(err))
         }
