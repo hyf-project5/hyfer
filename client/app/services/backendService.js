a528e0423fd264d9595b13756cf15a5273b85eb4
hasanshahoud <hasansh343@gmail.com>
2017-03-06 18:36:39 +0100

    lower case the userRole

--- client/app/services/backendService.js
+++ client/app/services/backendService.js
@@ -40,7 +40,6 @@
 
         function updateUserRole(userId, role) {
             return $http.patch('/api/user/' + userId, { "role": role }, getHttpConfig())
-                .then(res => res)
         }
 
     }
