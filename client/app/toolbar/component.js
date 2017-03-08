5910ac6e40ee2ecb43065ef810d9964fdf1500d3
hasanshahoud <hasansh343@gmail.com>
2017-03-08 12:29:18 +0100

    signin&signout functionality in the toolbar and deleting the previous login page(with it's styling!

--- client/app/toolbar/component.js
+++ client/app/toolbar/component.js
@@ -4,19 +4,32 @@
     class hyfToolbarCtrl {
 
         static get $inject() {
-            return ['me'];
+            return ['me', '$cookies', '$state'];
         }
 
-        constructor(me) {
+        constructor(me, $cookies, $state) {
             this.me = me;
+            this.$cookies = $cookies;
+            this.$state = $state;
         }
 
         notTeacher() {
             if (this.me.role !== 'teacher') {
                 return true;
             }
+
         }
 
+        signin() {
+            window.location.href = `/auth/github`
+        }
+
+        signout() {
+            window.localStorage.removeItem('token');
+            this.$cookies.remove('token');
+            location.reload();
+            this.$state.go('timeline')
+        }
 
     }
 
