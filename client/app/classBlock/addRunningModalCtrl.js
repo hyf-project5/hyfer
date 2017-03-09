ec3016f3847fff21aa4637f96603d90bfcd35237
hasanshahoud <hasansh343@gmail.com>
2017-03-10 00:02:57 +0100

    implemented addRunningModule ctrl for the modal

--- client/app/classBlock/addRunningModalCtrl.js
+++ client/app/classBlock/addRunningModalCtrl.js
@@ -4,17 +4,18 @@
     class addRunningModuleCtrl {
 
         static get $inject() {
-            return ['$mdDialog', 'backendService'];
+            return ['$mdDialog', 'backendService', 'className'];
         }
 
-        constructor($mdDialog, backendService) {
+        constructor($mdDialog, backendService, className) {
             this.$mdDialog = $mdDialog;
             this.backendService = backendService;
             backendService.getTimeline()
                 .then(data => {
                     this.timeline = data;
                     this.classNames = Object.keys(this.timeline).sort();
-                    this.runningModules = this.timeline[this.classNames[0]];
+                    this.runningModules = this.timeline[className];
+                    console.log(this.runningModules)
                 })
                 .catch(err => console.log(err));
             this.users = [];
@@ -31,8 +32,49 @@
         cancel() {
             this.$mdDialog.cancel()
         }
+
         add(submit) {
-            this.$mdDialog.hide(submit)
+            this.runningModules.forEach(module => {
+                if (module.module_name === submit.module_name) {
+                    this.$mdDialog.hide(submit)
+                        .then(res => {
+                            this.getModuleId(submit.module_name)
+                                .then(id => {
+                                    this.getModulePosition(submit.afterModule)
+                                        .then(position => {
+                                            position++
+                                            console.log(position)
+                                            this.backendService.addRunningModule(id, module.id, position)
+                                                .then(res => console.log(res))
+                                                .catch(err => console.log(err))
+                                        })
+                                })
+                        })
+                }
+            })
+        }
+
+        getModuleId(module_name) {
+            return new Promise(resolve => {
+                this.backendService.getModules()
+                    .then(modules => {
+                        modules.forEach(val => {
+                            if (val.module_name === module_name) {
+                                resolve(val.id)
+                            }
+                        })
+                    })
+            })
+        }
+
+        getModulePosition(afterModule) {
+            return new Promise(resolve => {
+                this.runningModules.forEach((module, position) => {
+                    if (module.module_name === afterModule) {
+                        resolve(position);
+                    }
+                })
+            })
         }
 
     }
