aedc1b761afc42a38fa195fe0a1e76505f6b3fcb
hasanshahoud <hasansh343@gmail.com>
2017-03-12 14:58:41 +0100

    Fixed class letter to upper case

--- client/app/timeline/component.js
+++ client/app/timeline/component.js
@@ -33,9 +33,11 @@
                     clickOutsideToClose: true
                 })
                 .then(group => {
-                    this.backendService.addGroup(group)
+                    let classInfo = group.group_name.split('')[0].toUpperCase() + group.group_name.split('').slice(1).join('');
+                    classInfo = { group_name: classInfo, starting_date: group.starting_date };
+                    this.backendService.addGroup(classInfo)
                         .then((res) => {
-                            this.toastService.displayToast(true, `${group.group_name} has been added`);
+                            this.toastService.displayToast(true, `${classInfo.group_name} has been added`);
                             this.$state.reload();
                         })
                 })
