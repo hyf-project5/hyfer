c1f8ad7ab0d55344d6d530dbe94f842c59f70343
Jim Cramer <remarcmij@gmail.com>
2017-03-31 17:01:39 +0200

    split up modules

---
+++ client/app/modules/modules-list-item-menu.component.js
@@ -0,0 +1,50 @@
+import angular from 'angular';
+import modulesModule from './modules.module';
+
+import addAndUpdateModuleController from '../modals/modules/addAndUpdateModuleModalCtrl';
+import template from './modules-list-item-menu.component.html';
+import updateModuleTemplate from '../modals/modules/updateModuleModal.html';
+
+class ModulesListItemMenuController {
+
+    static get $inject() {
+        return ['$mdDialog'];
+    }
+
+    constructor($mdDialog) {
+        this.$mdDialog = $mdDialog;
+    }
+
+    delete() {
+        this.onDelete({ module: this.module });
+    }
+
+    updateModule(ev) {
+        this.$mdDialog.show({
+            locals: { selectedModule: this.module },
+            controller: addAndUpdateModuleController,
+            controllerAs: '$ctrl',
+            template: updateModuleTemplate,
+            targetEvent: ev,
+            clickOutsideToClose: true
+        }).then(module => {
+            Object.assign(this.module, module);
+            this.onChanged();
+        });
+    }
+}
+
+const componentName = 'hyfModulesListItemMenu';
+
+angular.module(modulesModule)
+    .component(componentName, {
+        template,
+        controller: ModulesListItemMenuController,
+        bindings: {
+            module: '<',
+            onChanged: '&',
+            onDelete: '&'
+        }
+    });
+
+export default componentName;
--- client/app/modules/modules.component.js
+++ client/app/modules/modules.component.js
@@ -2,12 +2,14 @@ import angular from 'angular';
 import modulesModule from './modules.module';
 
 import '../footer/footer.component';
+import './modules-header.component';
+import './modules-list.component';
+
 import backendService from '../services/backendService';
 import toastService from '../services/toastService';
 import addAndUpdateModuleController from '../modals/modules/addAndUpdateModuleModalCtrl';
 import template from './modules.component.html';
 import addModuleTemplate from '../modals/modules/addModuleModal.html';
-import updateModuleTemplate from '../modals/modules/updateModuleModal.html';
 
 class ModulesController {
 
@@ -20,45 +22,42 @@ class ModulesController {
         this.$mdDialog = $mdDialog;
         this.toastService = toastService;
         this.$state = $state;
-        this.weeksGrid = ["1 week","2 weeks","3 weeks","4 weeks","5 weeks","6 weeks"];
         this.isDirty = false;
     }
 
-    save(){
+    save() {
         this.backendService.saveModules(this.modules)
-        .then(modules => {
-            this.isDirty= false;
-            this.modules = modules;
-            this.toastService.displayToast(true, 'Changes have been saved');
-        });
+            .then(modules => {
+                this.isDirty = false;
+                this.modules = modules;
+                this.toastService.displayToast(true, 'Changes have been saved');
+            });
     }
 
-    delete(index,name){
-        this.toastService.displayToast(false, `${name} have been deleted`);
-        this.modules.splice(index, 1);
+    onChanged(modules) {
+        this.modules = modules;
         this.isDirty = true;
-        
-   
-}
+    }
+
     undoChanges() {
         this.$state.reload();
-        this.isDirty= false;
-        setTimeout(()=>{
+        this.isDirty = false;
+        setTimeout(() => {
             this.toastService.displayToast(true, 'Changes have been rolled back');
-        },10)
+        }, 10);
 
     }
     addModule(ev) {
         this.$mdDialog.show({
-                locals: {
-                    selectedModule: null
-                },
-                controller: addAndUpdateModuleController,
-                controllerAs: '$ctrl',
-                template: addModuleTemplate,
-                targetEvent: ev,
-                clickOutsideToClose: true
-            })
+            locals: {
+                selectedModule: null
+            },
+            controller: addAndUpdateModuleController,
+            controllerAs: '$ctrl',
+            template: addModuleTemplate,
+            targetEvent: ev,
+            clickOutsideToClose: true
+        })
             .then(module => {
                 this.modules.push(module);
                 this.toastService.displayToast(true, `${module.module_name} has been added`);
@@ -66,22 +65,6 @@ class ModulesController {
             })
             .catch(err => console.log(err));
     }
-
-    updateModule(ev, selectedModule) {
-        this.$mdDialog.show({
-            locals: {  selectedModule  },
-            controller: addAndUpdateModuleController,
-            controllerAs: '$ctrl',
-            template: updateModuleTemplate,
-            targetEvent: ev,
-            clickOutsideToClose: true
-        })
-        .then(module => {
-            Object.assign(selectedModule, module);
-            this.isDirty = true;
-         });
-    }
-
 }
 
 const componentName = 'hyfModules';
