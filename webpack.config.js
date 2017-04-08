dd71e9423b90851a0e33d54ef0deb0add7e1c262
Jim Cramer <remarcmij@gmail.com>
2017-04-08 23:39:32 +0200

    fixed issue with image-webpack-loader

--- webpack.config.js
+++ webpack.config.js
@@ -14,40 +14,42 @@ module.exports = {
     devtool: 'source-map',
     module: {
         rules: [{
-                test: /\.js$/,
-                loader: 'babel-loader',
-                include: __dirname + '/client',
-                query: {
-                    plugins: ['transform-runtime']
-                }
-            },
-            {
-                test: /\.html$/,
-                use: 'html-loader'
-            },
-            {
-                test: /\.css$/,
-                use: ExtractTextPlugin.extract({ use: 'css-loader' })
-            },
-            {
-                test: /\.(jpe?g|png|gif|svg)$/,
-                use: [{
-                        loader: 'url-loader',
-                        options: { limit: 40000 }
-                    },
-                    'image-webpack-loader'
-                ]
-            },
-            {
-                test: /\.scss$/,
-                use: [{
-                    loader: "style-loader" // creates style nodes from JS strings
-                }, {
-                    loader: "css-loader" // translates CSS into CommonJS
-                }, {
-                    loader: "sass-loader" // compiles Sass to CSS
-                }]
+            test: /\.js$/,
+            loader: 'babel-loader',
+            include: __dirname + '/client',
+            query: {
+                plugins: ['transform-runtime']
             }
+        },
+        {
+            test: /\.html$/,
+            use: 'html-loader'
+        },
+        {
+            test: /\.css$/,
+            use: ExtractTextPlugin.extract({ use: 'css-loader' })
+        },
+        {
+            test: /\.(jpe?g|png|gif|svg)$/,
+            use: [{
+                loader: 'url-loader',
+                options: { limit: 40000 }
+            },
+            {
+                loader: 'image-webpack-loader',
+                options: {}
+            }]
+        },
+        {
+            test: /\.scss$/,
+            use: [{
+                loader: "style-loader" // creates style nodes from JS strings
+            }, {
+                loader: "css-loader" // translates CSS into CommonJS
+            }, {
+                loader: "sass-loader" // compiles Sass to CSS
+            }]
+        }
         ]
     },
     plugins: [new ExtractTextPlugin('style.css')]
