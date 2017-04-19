e6f1d0a70e1cad09f7d720d9d113efbe3145eba0
hasanshahoud <hasansh343@gmail.com>
2017-04-19 11:19:56 +0200

    created addclass modal controller, and fixed some typos

---
+++ client/app/modals/classes/addClass.controller.js
@@ -0,0 +1,36 @@
+import angular from 'angular';
+
+import modalsModules from '../modals.module';
+import toastService from '../../services/toastService';
+
+class AddClassModalController {
+
+    static get $inject() {
+        return ['$mdDialog', toastService, '$state'];
+    }
+
+    constructor($mdDialog, toastService, $state) {
+        this.$mdDialog = $mdDialog;
+        this.toastService = toastService;
+        this.$state = $state;
+    }
+
+    cancel() {
+        this.$mdDialog.cancel();
+        setTimeout(() => {
+            this.toastService.displayToast(false);
+        }, 10);
+    }
+
+    add(group) {
+        this.$mdDialog.hide(group);
+    }
+
+}
+
+const controllerName = 'AddClassModalController';
+
+angular.module(modalsModules)
+    .controller(controllerName, AddClassModalController);
+
+export default controllerName;
--- client/app/timeline/timeline.component.js
+++ client/app/timeline/timeline.component.js
@@ -6,7 +6,7 @@ import './classBlock/classBlock.component';
 import '../footer/footer.component';
 import backendService from '../services/backendService';
 import toastService from '../services/toastService';
-import addAndUpdateModuleController from '../modals/modules/addAndUpdateModuleModalCtrl';
+import AddClassModalController from '../modals/classes/addClass.controller';
 import timelineTemplate from './timeline.component.html';
 import addClassTemplate from '../modals/classes/addClassModal.html';
 
@@ -36,13 +36,14 @@ class TimelineController {
                 locals: {
                     selectedModule: null
                 },
-                controller: addAndUpdateModuleController,
+                controller: AddClassModalController,
                 controllerAs: '$ctrl',
                 template: addClassTemplate,
                 targetEvent: ev,
                 clickOutsideToClose: true
             })
             .then(group => {
+                console.log('group: ', group);
                 let classInfo = group.group_name.split('')[0].toUpperCase() + group.group_name.split('').slice(1).join('');
                 classInfo = { group_name: classInfo, starting_date: group.starting_date };
                 this.backendService.addGroup(classInfo)
