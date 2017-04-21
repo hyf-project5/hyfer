2dfd92c0c44a2fc92d45d403c357510055bf97ee
Jim Cramer <remarcmij@gmail.com>
2017-04-21 19:51:43 +0200

    update sample data

--- sql/sample.data.sql
+++ sql/sample.data.sql
@@ -7,7 +7,7 @@
 #
 # Host: localhost (MySQL 5.7.16)
 # Database: hyfer
-# Generation Time: 2017-04-20 12:08:54 +0000
+# Generation Time: 2017-04-21 17:51:13 +0000
 # ************************************************************
 
 
@@ -51,12 +51,12 @@ LOCK TABLES `modules` WRITE;
 
 INSERT INTO `modules` (`id`, `module_name`, `description`, `added_on`, `default_duration`, `sort_order`, `git_url`, `git_owner`, `git_repo`, `color`, `optional`)
 VALUES
-	(1,'Angular JS','Angular JS module description','2017-02-28 18:25:16',3,4,'https://github.com/HackYourFuture/','HackYourFuture','angular','#375e97',0),
+	(1,'Angular JS','Angular JS module description','2017-02-28 18:25:16',3,5,'https://github.com/HackYourFuture/','HackYourFuture','angular','#375e97',0),
 	(2,'HTML-CSS','HTML-CSS module description','2017-02-28 18:25:56',3,0,'https://github.com/HackYourFuture/','HackYourFuture','angular','#fb6542',0),
 	(3,'JavaScript 1','JavaScript 1 module description','2017-02-28 18:26:27',3,1,'https://github.com/HackYourFuture/','HackYourFuture','JavaScript','#ffbb00',0),
 	(4,'JavaScript 2','JavaScript 2 module description','2017-02-28 18:26:41',3,2,'https://github.com/HackYourFuture/','HackYourFuture','JavaScript2','#3f681c',0),
 	(5,'JavaScript 3','JavaScript 3 module description','2017-02-28 18:26:49',3,3,'https://github.com/HackYourFuture/','HackYourFuture','JavaScript3','#eb8a44',0),
-	(6,'Node.js','Node.js module description','2017-02-28 18:27:21',3,5,'https://github.com/HackYourFuture/','HackYourFuture','Node.js','#f9dc24',0),
+	(6,'Node.js','Node.js module description','2017-02-28 18:27:21',3,4,'https://github.com/HackYourFuture/','HackYourFuture','Node.js','#f9dc24',0),
 	(7,'Databases','Databases module description','2017-02-28 18:27:50',3,6,'https://github.com/HackYourFuture/','HackYourFuture','databases','#4b7447',0),
 	(8,'Project','Project module description','2017-02-28 18:28:09',6,7,'https://github.com/HackYourFuture/','HackYourFuture','Project','#8eba43',0),
 	(9,'Holiday','Holiday period','2017-03-06 14:35:26',1,1000,NULL,NULL,NULL,'#c05805',1),
@@ -68,19 +68,94 @@ UNLOCK TABLES;
 
 # Dump of table running_modules
 # ------------------------------------------------------------
+
 LOCK TABLES `running_modules` WRITE;
 /*!40000 ALTER TABLE `running_modules` DISABLE KEYS */;
-INSERT INTO `running_modules` VALUES (106,'HTML-CSS module description',2,43,3,0,NULL,NULL),(107,'JavaScript 1 module description',3,43,3,1,NULL,NULL),(108,'JavaScript 2 module description',4,43,3,2,NULL,NULL),(109,'JavaScript 3 module description',5,43,3,3,NULL,NULL),(110,'Angular JS module description',1,43,3,4,NULL,NULL),(111,'Node.js module description',6,43,6,5,NULL,NULL),(112,'Databases module description',7,43,3,8,NULL,NULL),(113,'Project module description',8,43,6,9,NULL,NULL),(114,'HTML-CSS module description',2,44,3,0,NULL,NULL),(115,'JavaScript 1 module description',3,44,3,1,NULL,NULL),(116,'JavaScript 2 module description',4,44,3,2,NULL,NULL),(117,'JavaScript 3 module description',5,44,3,3,NULL,NULL),(118,'Angular JS module description',1,44,3,6,NULL,NULL),(119,'Node.js module description',6,44,3,7,NULL,NULL),(120,'Databases module description',7,44,3,8,NULL,NULL),(121,'Project module description',8,44,6,9,NULL,NULL),(122,'HTML-CSS module description',2,45,3,0,NULL,NULL),(123,'JavaScript 1 module description',3,45,3,1,NULL,NULL),(124,'JavaScript 2 module description',4,45,3,4,NULL,NULL),(125,'JavaScript 3 module description',5,45,3,5,NULL,NULL),(126,'Angular JS module description',1,45,3,6,NULL,NULL),(127,'Node.js module description',6,45,3,7,NULL,NULL),(128,'Databases module description',7,45,3,8,NULL,NULL),(129,'Project module description',8,45,6,9,NULL,NULL),(130,'HTML-CSS module description',2,46,3,0,NULL,NULL),(131,'JavaScript 1 module description',3,46,3,1,NULL,NULL),(132,'JavaScript 2 module description',4,46,3,2,NULL,NULL),(133,'JavaScript 3 module description',5,46,3,3,NULL,NULL),(134,'Angular JS module description',1,46,3,4,NULL,NULL),(135,'Node.js module description',6,46,3,5,NULL,NULL),(136,'Databases module description',7,46,3,6,NULL,NULL),(137,'Project module description',8,46,6,7,NULL,NULL),(138,'HTML-CSS module description',2,47,3,0,NULL,NULL),(139,'JavaScript 1 module description',3,47,3,1,NULL,NULL),(140,'JavaScript 2 module description',4,47,3,2,NULL,NULL),(141,'JavaScript 3 module description',5,47,3,3,NULL,NULL),(142,'Angular JS module description',1,47,3,4,NULL,NULL),(143,'Node.js module description',6,47,3,5,NULL,NULL),(144,'Databases module description',7,47,3,6,NULL,NULL),(145,'Project module description',8,47,6,7,NULL,NULL),(146,'Holiday',9,43,2,7,NULL,NULL),(147,'Holiday',9,44,2,5,NULL,NULL),(148,'Holiday',9,45,2,3,NULL,NULL),(149,'Hackathon',10,43,1,6,NULL,NULL),(150,'Hackathon',10,44,1,4,NULL,NULL),(151,'Hackathon',10,45,1,2,NULL,NULL);
+
+INSERT INTO `running_modules` (`id`, `description`, `module_id`, `group_id`, `duration`, `position`, `teacher1_id`, `teacher2_id`)
+VALUES
+	(106,'HTML-CSS module description',2,43,3,0,NULL,NULL),
+	(107,'JavaScript 1 module description',3,43,3,1,NULL,NULL),
+	(108,'JavaScript 2 module description',4,43,3,2,NULL,NULL),
+	(109,'JavaScript 3 module description',5,43,3,3,NULL,NULL),
+	(110,'Angular JS module description',1,43,3,4,NULL,NULL),
+	(111,'Node.js module description',6,43,6,5,NULL,NULL),
+	(112,'Databases module description',7,43,3,8,NULL,NULL),
+	(113,'Project module description',8,43,6,9,NULL,NULL),
+	(146,'Holiday',9,43,2,7,NULL,NULL),
+	(149,'Hackathon',10,43,1,6,NULL,NULL),
+	(163,'HTML-CSS module description',2,44,3,0,NULL,NULL),
+	(164,'JavaScript 1 module description',3,44,3,1,NULL,NULL),
+	(165,'JavaScript 2 module description',4,44,3,2,NULL,NULL),
+	(166,'JavaScript 3 module description',5,44,3,3,NULL,NULL),
+	(167,'Hackathon',10,44,1,4,NULL,NULL),
+	(168,'Holiday',9,44,2,5,NULL,NULL),
+	(169,'Angular JS module description',1,44,3,6,NULL,NULL),
+	(170,'Node.js module description',6,44,3,7,NULL,NULL),
+	(171,'Databases module description',7,44,3,8,NULL,NULL),
+	(172,'Project module description',8,44,3,9,NULL,NULL),
+	(173,'Hackathon / fun day',10,44,1,10,NULL,NULL),
+	(174,'Project module description',8,44,3,11,NULL,NULL),
+	(175,'HTML-CSS module description',2,45,3,0,NULL,NULL),
+	(176,'JavaScript 1 module description',3,45,3,1,NULL,NULL),
+	(177,'Hackathon',10,45,1,2,NULL,NULL),
+	(178,'Holiday',9,45,2,3,NULL,NULL),
+	(179,'JavaScript 2 module description',4,45,3,4,NULL,NULL),
+	(180,'JavaScript 3 module description',5,45,3,5,NULL,NULL),
+	(181,'Angular JS module description',1,45,3,6,NULL,NULL),
+	(182,'Node.js module description',6,45,3,7,NULL,NULL),
+	(183,'Hackathon / fun day',10,45,1,8,NULL,NULL),
+	(184,'Databases module description',7,45,3,9,NULL,NULL),
+	(185,'Project module description',8,45,6,10,NULL,NULL),
+	(213,'HTML-CSS module description',2,46,3,0,NULL,NULL),
+	(214,'JavaScript 1 module description',3,46,3,1,NULL,NULL),
+	(215,'JavaScript 2 module description',4,46,3,2,NULL,NULL),
+	(216,'Hackathon / fun day',10,46,1,3,NULL,NULL),
+	(217,'JavaScript 3 module description',5,46,3,4,NULL,NULL),
+	(218,'Node.js module description',6,46,3,5,NULL,NULL),
+	(219,'Angular JS module description',1,46,3,6,NULL,NULL),
+	(220,'Databases module description',7,46,3,7,NULL,NULL),
+	(221,'Project module description',8,46,6,8,NULL,NULL),
+	(222,'HTML-CSS module description',2,47,3,0,NULL,NULL),
+	(223,'Hackathon / fun day',10,47,1,1,NULL,NULL),
+	(224,'JavaScript 1 module description',3,47,3,2,NULL,NULL),
+	(225,'JavaScript 2 module description',4,47,3,3,NULL,NULL),
+	(226,'JavaScript 3 module description',5,47,3,4,NULL,NULL),
+	(227,'Node.js module description',6,47,3,5,NULL,NULL),
+	(228,'Angular JS module description',1,47,3,6,NULL,NULL),
+	(229,'Databases module description',7,47,3,7,NULL,NULL),
+	(230,'Project module description',8,47,6,8,NULL,NULL);
+
 /*!40000 ALTER TABLE `running_modules` ENABLE KEYS */;
 UNLOCK TABLES;
+
 
 # Dump of table students_history
 # ------------------------------------------------------------
 
-
 LOCK TABLES `students_history` WRITE;
 /*!40000 ALTER TABLE `students_history` DISABLE KEYS */;
-INSERT INTO `students_history` VALUES (47,139,20,'2017-03-12',0,0),(47,139,21,'2017-03-12',0,1),(47,139,22,'2017-03-12',0,1),(47,139,23,'2017-03-12',0,1),(47,139,24,'2017-03-12',0,1),(47,139,25,'2017-03-12',1,0),(47,139,26,'2017-03-12',1,1),(47,139,27,'2017-03-12',1,0),(47,139,28,'2017-03-12',0,1),(46,133,30,'2017-03-12',1,1),(46,133,31,'2017-03-12',0,1),(46,133,32,'2017-03-12',0,1),(46,133,33,'2017-03-12',0,1),(46,133,34,'2017-03-12',0,1),(46,133,35,'2017-03-12',0,1),(46,133,36,'2017-03-12',0,1),(46,133,37,'2017-03-12',0,1),(46,133,38,'2017-03-12',0,1),(46,133,39,'2017-03-12',0,1),(45,112,40,'2016-11-06',0,0),(45,112,41,'2016-11-06',0,0),(45,112,42,'2016-11-06',0,0),(45,112,43,'2016-11-06',0,0),(45,112,44,'2016-11-06',0,0),(45,112,45,'2016-11-06',0,0),(45,112,46,'2016-11-06',0,0),(44,112,47,'2016-11-06',0,0),(44,113,48,'2016-08-25',0,0),(44,113,49,'2016-08-25',0,0),(44,113,50,'2016-08-25',0,0),(44,113,51,'2016-08-25',0,0),(44,113,52,'2016-08-25',0,0),(44,113,53,'2016-08-25',0,0),(44,113,54,'2016-08-25',0,0),(44,113,55,'2016-08-25',0,0),(44,112,56,'2016-08-25',0,0);
+
+INSERT INTO `students_history` (`group_id`, `running_module_id`, `user_id`, `date`, `attendance`, `homework`)
+VALUES
+	(45,112,40,'2016-11-06',0,0),
+	(45,112,41,'2016-11-06',0,0),
+	(45,112,42,'2016-11-06',0,0),
+	(45,112,43,'2016-11-06',0,0),
+	(45,112,44,'2016-11-06',0,0),
+	(45,112,45,'2016-11-06',0,0),
+	(45,112,46,'2016-11-06',0,0),
+	(44,112,47,'2016-11-06',0,0),
+	(44,113,48,'2016-08-25',0,0),
+	(44,113,49,'2016-08-25',0,0),
+	(44,113,50,'2016-08-25',0,0),
+	(44,113,51,'2016-08-25',0,0),
+	(44,113,52,'2016-08-25',0,0),
+	(44,113,53,'2016-08-25',0,0),
+	(44,113,54,'2016-08-25',0,0),
+	(44,113,55,'2016-08-25',0,0),
+	(44,112,56,'2016-08-25',0,0);
+
 /*!40000 ALTER TABLE `students_history` ENABLE KEYS */;
 UNLOCK TABLES;
 
