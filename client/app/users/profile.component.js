dbaca4dbfa79bfa1d4fd80e150cab270fda44b7b
hasanshahoud <hasansh343@gmail.com>
2017-04-27 16:00:30 +0200

    Get the classes names with their ids, and show it i the md-select element

--- client/app/users/profile.component.js
+++ client/app/users/profile.component.js
@@ -6,41 +6,47 @@ import template from './profile.component.html';
 import toolbarService from '../toolbar/toolbar.service';
 import freecodecamp from '../../assets/images/freecodecamp.svg';
 
-class ProfileController{
+class ProfileController {
 
     static get $inject() {
-        return [toolbarService];
+        return ['$timeout', toolbarService, backendService];
     }
-    
-    constructor(toolbarService){
+
+    constructor($timeout, toolbarService, backendService) {
         this.toolbarService = toolbarService;
         this.toolbarService.switchToChild({
             title: 'Edit Profile',
             backState: 'users'
         });
-        this.freecodecamp =freecodecamp;
-        this.classes=[
-            {
-                name: 'class 5'
-            },{
-                name: 'class 6'
-            },{
-                name: 'class 7'
-            },{
-                name: 'class 8'
-            },{
-                name: 'class 9'
-            }
-        ];
+        this.backendService = backendService;
+        this.$timeout = $timeout;
+        this.freecodecamp = freecodecamp;
+    }
+
+    getGroupsNameId() {
+        return this.$timeout(() => {
+            this.backendService.getTimeline()
+                .then(res => {
+                    let classesName = Object.entries(res);
+                    this.classes = classesName.reduce((acc, curr) => {
+                        let classNameAndId = {
+                            name: curr[0],
+                            group_id: curr[1][0].id
+                        }
+                        acc.push(classNameAndId);
+                        return acc;
+                    }, [])
+                    console.log(this.classes)
+                })
+        }, 400)
     }
 
 
-    
 }
 
- const componentName = 'hyfProfile';
+const componentName = 'hyfProfile';
 
-    angular
+angular
     .module(usersModule)
     .component(componentName, {
         template,
@@ -50,4 +56,4 @@ class ProfileController{
         }
     });
 
-    export default componentName;
+export default componentName;
